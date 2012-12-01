45e2aa2edba1591f4ca0486f99827ef3d6605577
if1live <4ipperz@gmail.com>
2012-12-01 21:08:42 +0900

    in game 좌표 변환에 사용할 클래스 추가

---
+++ sora/unit.cpp
@@ -0,0 +1,41 @@
+﻿// Ŭnicode please
+#include "sora_stdafx.h"
+#include "unit.h"
+
+SR_NS_BEGIN(sora);
+
+glm::vec2 Unit::ToUnitFromMeter(const b2Vec2 &mt_p) {
+    float ut_x = ToUnitFromMeter(mt_p.x);
+    float ut_y = ToUnitFromMeter(mt_p.y);
+    return glm::vec2(ut_x, ut_y);
+}
+
+float Unit::ToUnitFromMeter(float mt_length) {
+    Unit &unit = GetInstance();
+    float ut_length = mt_length * unit.mt_to_ut_scale();
+	return ut_length; 
+}
+
+b2Vec2 Unit::ToMeterFromUnit(const glm::vec2 &ut_p) {
+    float mt_x = ToMeterFromUnit(ut_p.x);
+    float mt_y = ToMeterFromUnit(ut_p.y);
+    return b2Vec2(mt_x, mt_y);
+}
+
+float Unit::ToMeterFromUnit(float ut_length) {
+    Unit &unit = GetInstance();
+    float mt_length = ut_length * unit.ut_to_mt_scale();
+    return mt_length;
+}
+
+Unit &Unit::GetInstance() {
+    static Unit ctx;
+    return ctx;
+}
+Unit::Unit()
+: px_width_(720), 
+px_height_(1280),
+mt_to_ut_scale_(kUnitToMeterRatio) {
+}
+
+SR_NS_END()
---
+++ sora/unit.h
@@ -0,0 +1,51 @@
+﻿// Ŭnicode please
+#pragma once
+
+class b2Vec2;
+
+SR_NS_BEGIN(sora);
+
+const float kUnitToMeterRatio = 32.0f;
+
+class Unit {
+public:
+    enum {
+        kWidth = 720,
+        kHeight = 1280,
+    };
+public:
+    static glm::vec2 ToUnitFromMeter(const b2Vec2 &mt_p);
+    static float ToUnitFromMeter(float mt_length);
+
+    static b2Vec2 ToMeterFromUnit(const glm::vec2 &ut_p);
+    static float ToMeterFromUnit(float ut_length);
+
+public:
+    static Unit &GetInstance();
+private:
+    Unit();
+
+    //픽셀 좌표계로써의 화면의 크기
+    float px_width_;
+    float px_height_;
+
+    //mt->unit scale
+    float mt_to_ut_scale_;
+    //unit->pt scale
+    //pt 좌표계가 따로 필요할지, 아니면 적절히 Unit를 전체에 대응되도록할지 알아서 정하기
+    //float ut_to_px_scale_;
+
+    //getter/setter
+public:
+    void set_px_width(float f) { px_width_ = f; }
+    float pt_width() const { return px_width_; }
+
+    void set_px_height(float f) { px_height_ = f; }
+    float px_height() const { return px_height_; }
+
+    void set_mt_to_ut_scale(float f) { mt_to_ut_scale_ = f; }
+    float mt_to_ut_scale() const { return mt_to_ut_scale_; }
+    float ut_to_mt_scale() const { return 1.0f / mt_to_ut_scale_; }
+};
+
+SR_NS_END()
