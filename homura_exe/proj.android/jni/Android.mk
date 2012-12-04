391e3016022dc69bfa667dde3c94eabd474c459e
if1live <4ipperz@gmail.com>
2012-12-04 22:16:25 +0900

    안드로이드 빌드 make 분리중

--- homura_exe/proj.android/jni/Android.mk
+++ homura_exe/proj.android/jni/Android.mk
@@ -8,43 +8,7 @@ LOCAL_MODULE_FILENAME := libhomura
 
 LOCAL_CPPFLAGS   +=  -fpermissive -Winvalid-offsetof
 
-#tinyxml
-LOCAL_SRC_FILES += \
-    ../../../sora/tinyxml/tinystr.cpp  \
-    ../../../sora/tinyxml/tinyxml.cpp  \
-    ../../../sora/tinyxml/tinyxmlerror.cpp  \
-    ../../../sora/tinyxml/tinyxmlparser.cpp  \
-    
-# sora
-LOCAL_SRC_FILES += \
-    ../../../sora/angle.cpp  \
-    ../../../sora/ani.cpp  \
-    ../../../sora/ani_data.cpp  \
-    ../../../sora/ani_frame.cpp  \
-    ../../../sora/ani_manager.cpp  \
-    ../../../sora/ani_node.cpp  \
-    ../../../sora/ani_parser.cpp  \
-    ../../../sora/ani_prototype.cpp  \
-    ../../../sora/ani_reader.cpp  \
-    ../../../sora/ani_writer.cpp  \
-    ../../../sora/class_attribute.cpp  \
-    ../../../sora/class_dict.cpp  \
-    ../../../sora/crc32.cpp  \
-    ../../../sora/data_define.cpp  \
-    ../../../sora/data_helper.cpp  \
-    ../../../sora/data_init.cpp  \
-    ../../../sora/logger.cpp  \
-    ../../../sora/math_helper.cpp  \
-    ../../../sora/mem.cpp  \
-    ../../../sora/string_hash.cpp  \
-    ../../../sora/string_util.cpp  \
-    ../../../sora/struct_version_converter.cpp  \
-    ../../../sora/vertex.cpp  \
-    ../../../sora/xml_node.cpp  \
-    ../../../sora/xml_writer.cpp  \
-    ../../../sora/xml_reader.cpp  \
-    ../../../sora/file_helper.cpp   \
-    ../../../sora/unit.cpp  \
+
                    
 # homura           
 LOCAL_SRC_FILES += \
@@ -87,9 +51,11 @@ LOCAL_C_INCLUDES := \
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
 LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
+LOCAL_WHOLE_STATIC_LIBRARIES += sora_static
 
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,cocos2dx)
 $(call import-module,CocosDenshion/android)
 $(call import-module,external/Box2D)
+$(call import-module,sora/jni)
---
+++ sora/jni/Android.mk
@@ -0,0 +1,51 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := sora_static
+
+LOCAL_MODULE_FILENAME := libsora
+#tinyxml
+LOCAL_SRC_FILES += \
+    ../tinyxml/tinystr.cpp  \
+    ../tinyxml/tinyxml.cpp  \
+    ../tinyxml/tinyxmlerror.cpp  \
+    ../tinyxml/tinyxmlparser.cpp  \
+    
+# sora
+LOCAL_SRC_FILES += \
+    ../angle.cpp  \
+    ../ani.cpp  \
+    ../ani_data.cpp  \
+    ../ani_frame.cpp  \
+    ../ani_manager.cpp  \
+    ../ani_node.cpp  \
+    ../ani_parser.cpp  \
+    ../ani_prototype.cpp  \
+    ../ani_reader.cpp  \
+    ../ani_writer.cpp  \
+    ../class_attribute.cpp  \
+    ../class_dict.cpp  \
+    ../crc32.cpp  \
+    ../data_define.cpp  \
+    ../data_helper.cpp  \
+    ../data_init.cpp  \
+    ../logger.cpp  \
+    ../math_helper.cpp  \
+    ../mem.cpp  \
+    ../string_hash.cpp  \
+    ../string_util.cpp  \
+    ../struct_version_converter.cpp  \
+    ../vertex.cpp  \
+    ../xml_node.cpp  \
+    ../xml_writer.cpp  \
+    ../xml_reader.cpp  \
+    ../file_helper.cpp   \
+    ../unit.cpp  \
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/..
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/..    \
+    $(LOCAL_PATH)/../../cocos2dx
+                                 
+include $(BUILD_STATIC_LIBRARY)
