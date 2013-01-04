9513d8669fada0f962968aeb92a5423e4652841f
Chulju, Hong <ang0123dev@gmail.com>
2013-01-05 01:11:04 +0900

    체인 그리기용 레이어 생성

---
+++ homura/chain_layer.cpp
@@ -0,0 +1,25 @@
+﻿// Ŭnicode please
+#include "stdafx.h"
+#include "chain_layer.h"
+
+USING_NS_CC;
+
+ChainLayer::ChainLayer(GameWorld *world) {
+    
+}
+
+ChainLayer::~ChainLayer() {
+
+}
+
+bool ChainLayer::init() {
+    if(!CCLayer::init()) {
+        return false;
+    }
+
+    return true;
+}
+
+void ChainLayer::draw() {
+
+}
---
+++ homura/chain_layer.h
@@ -0,0 +1,19 @@
+﻿// Ŭnicode please
+#ifndef __CHAIN_LAYER_H__
+#define __CHAIN_LAYER_H__
+
+#include "message_handleable.h"
+
+class GameWorld;
+
+class ChainLayer : public cocos2d::CCLayer, public MessageHandleable {
+public:
+    ChainLayer(GameWorld *world);
+    virtual ~ChainLayer();
+
+public:
+    bool init();
+    void draw();
+};
+
+#endif
